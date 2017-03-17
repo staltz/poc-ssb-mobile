6de010ba8ccf95f5aeec094ee810a8e4244f71cd
Andre Staltz <andre@staltz.com>
2017-03-17 23:38:40 +0200

    Use ssb-keys to generate and display a key

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
-import React from 'react';
-// import * as ssbKeys from 'ssb-keys';
-import { StyleSheet, Text, View } from 'react-native';
 import './install-globals';
-const path = require('path');
+import React from 'react';
+import { StyleSheet, Text, View, ToastAndroid } from 'react-native';
+import * as ssbkeys from 'ssb-keys';
 
-const b = new Buffer('foobar');
+var myKeys = ssbkeys.generate();
 
 export default class App extends React.Component {
+  componentDidMount() {
+    // ToastAndroid.show('hi', ToastAndroid.SHORT);
+    // ToastAndroid.show(JSON.stringify(sodium), ToastAndroid.LONG);
+    // ToastAndroid.show(JSON.stringify(chloride.to_hex), ToastAndroid.LONG);
+    // ToastAndroid.show('' + sodium.to_hex, ToastAndroid.LONG);
+    // ToastAndroid.show(sodium.to_hex.toString(), ToastAndroid.SHORT);
+    // ToastAndroid.show(sodium.crypto_generichash.toString(), ToastAndroid.SHORT);
+    // const thing = sodium.to_hex(sodium.crypto_generichash(64, 'test'));
+    // ToastAndroid.show(thing, ToastAndroid.LONG);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>key: {153123}</Text>
+        <Text style={{fontSize: 20}}>key: {myKeys.id}</Text>
       </View>
     );
   }
@@ -19,7 +29,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffc',
     alignItems: 'center',
     justifyContent: 'center',
   },
