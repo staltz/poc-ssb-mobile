e560f7337afa38ef127483ea58732c562bba086e
Andre Staltz <andre@staltz.com>
2017-03-22 18:09:06 +0200

    Use react-native-network-info to get real LAN IP

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import './install-globals';
 import React from 'react';
 import { StyleSheet, Text, View, ToastAndroid, AsyncStorage } from 'react-native';
 import createSbot from './sbot';
+import NetworkInfo from 'react-native-network-info';
 const ssbKeys = require('ssb-keys');
 const path = require('path');
 const pull = require('pull-stream');
@@ -14,73 +15,75 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    ssbKeys.loadOrCreate(path.join('~/.ssb', 'secret'), (err, keys) => {
-      if (err) {
-        ToastAndroid.show(err, ToastAndroid.LONG)
-      } else {
-        this.setState((prevState, props) => { return {keys} })
-        // create a scuttlebot client using default settings
-        // (server at localhost:8080, using key found at ~/.ssb/secret)
-        config.keys = keys
-        config.host = '192.168.1.104'
-        console.log(keys.id)
-        const sbot = createSbot(config)
+    NetworkInfo.getIPAddress(ip => {
+      ssbKeys.loadOrCreate(path.join('~/.ssb', 'secret'), (err, keys) => {
+        if (err) {
+          ToastAndroid.show(err, ToastAndroid.LONG)
+        } else {
+          this.setState((prevState, props) => { return {keys} })
+          // create a scuttlebot client using default settings
+          // (server at localhost:8080, using key found at ~/.ssb/secret)
+          config.keys = keys
+          config.host = ip
+          console.log(keys.id)
+          const sbot = createSbot(config)
 
-        AsyncStorage.getAllKeys((err, keys) => {
-          if (err) {
-            console.error(err)
-          } else {
-            console.log('AsyncStorage getAllKeys', keys);
-            keys.forEach(key => {
-              AsyncStorage.getItem(key, (err2, value) => {
-                console.log('AsyncStorage', key, value)
+          AsyncStorage.getAllKeys((err, keys) => {
+            if (err) {
+              console.error(err)
+            } else {
+              console.log('AsyncStorage getAllKeys', keys);
+              keys.forEach(key => {
+                AsyncStorage.getItem(key, (err2, value) => {
+                  console.log('AsyncStorage', key, value)
+                })
               })
-            })
-          }
-        })
-
-        // stream all messages in all feeds, ordered by receive time
-        pull(
-          sbot.createFeedStream(),
-          pull.collect(function (err, msgs) {
-            console.log('createFeedStream', err, msgs)
-            // ToastAndroid.show(msgs, ToastAndroid.LONG);
-            // msgs[0].key == hash(msgs[0].value)
-            // msgs[0].value...
+            }
           })
-        )
 
-        console.log('gossip.peers', sbot.gossip.peers())
+          // stream all messages in all feeds, ordered by receive time
+          pull(
+            sbot.createFeedStream(),
+            pull.collect(function (err, msgs) {
+              console.log('createFeedStream', err, msgs)
+              // ToastAndroid.show(msgs, ToastAndroid.LONG);
+              // msgs[0].key == hash(msgs[0].value)
+              // msgs[0].value...
+            })
+          )
 
-        sbot.gossip.changes(function (err, changes) {
-          console.log('gossip.changes', changes)
-        })
+          console.log('gossip.peers', sbot.gossip.peers())
 
-        sbot.gossip.add(
-          '192.168.1.107:8008:@/AM2aoINDzNh6OwpOmT6BlC+CpSelAPDZL+ihjdyQXo=.ed25519',
-          'local'
-        )
+          sbot.gossip.changes(function (err, changes) {
+            console.log('gossip.changes', changes)
+          })
 
-        sbot.replicate.changes(function (err, changes) {
-          console.log('replicate.changes', changes)
-        })
+          sbot.gossip.add(
+            '192.168.1.107:8008:@/AM2aoINDzNh6OwpOmT6BlC+CpSelAPDZL+ihjdyQXo=.ed25519',
+            'local'
+          )
 
-        sbot.friends.all('follow', function (err, friends) {
-          console.log('friends.all', JSON.stringify(friends, null, '  '))
-        })
+          sbot.replicate.changes(function (err, changes) {
+            console.log('replicate.changes', changes)
+          })
 
-        sbot.publish({ type: 'post', text: 'First Post from Android!' }, function (err, msg) {
-          if (err) {
-            console.error(err)
-          } else {
-            console.log('sbot publish', msg)
-          }
-          // msg.key           == hash(msg.value)
-          // msg.value.author  == your id
-          // msg.value.content == { type: 'post', text: 'My First Post!' }
-          // ...
-        })
-      }
+          sbot.friends.all('follow', function (err, friends) {
+            console.log('friends.all', JSON.stringify(friends, null, '  '))
+          })
+
+          sbot.publish({ type: 'post', text: 'First Post from Android!' }, function (err, msg) {
+            if (err) {
+              console.error(err)
+            } else {
+              console.log('sbot publish', msg)
+            }
+            // msg.key           == hash(msg.value)
+            // msg.value.author  == your id
+            // msg.value.content == { type: 'post', text: 'My First Post!' }
+            // ...
+          })
+        }
+      })
     })
   }
 
--- android/app/src/main/java/com/pocandroid/MainApplication.java
+++ android/app/src/main/java/com/pocandroid/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.pusherman.networkinfo.RNNetworkInfoPackage;
 import com.tradle.react.UdpSocketsModule;
 import com.peel.react.TcpSocketsModule;
 import com.bitgo.randombytes.RandomBytesPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNNetworkInfoPackage(),
             new UdpSocketsModule(),
             new TcpSocketsModule(),
             new RandomBytesPackage()
